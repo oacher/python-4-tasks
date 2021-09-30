accae9d0604635b896a53e863223c75630572ecf
oacher <ochierniatieva@mail.ru>
2021-09-30 13:07:10 +0300

    first commit

---
+++ 1.py
@@ -0,0 +1,10 @@
+surname = "Иванов"
+name = "Иван"
+data = "1900"
+print(name + " " + surname + " " + data)
+x = surname
+surname = name
+name = x
+data = int(data)+60
+data = str(data)
+print(name + " " + surname+ " " + data)
